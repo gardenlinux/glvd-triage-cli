b3e288466209cd8645002922bd6499cff2c26179
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-12-17 12:28:54 +0100

    improve tests

--- asserts-entrypoint.sh
+++ asserts-entrypoint.sh
@@ -3,8 +3,13 @@
 echo "$PGHOST:$PGPORT:$PGDATABASE:$PGUSER:$PGPASSWORD" > ~/.pgpass
 chmod 0600 ~/.pgpass
 
+# todo(fwilhe): better assertions, more stable and useful test data
+
 psql -c "select * from public.cve_context where create_date > now() - interval '1 day';" glvd > /tmp/results.txt
 
-if grep -q 'CVE-' /tmp/results.txt; then
-  echo "ok"
+if grep -q -E 'CVE-2024-10979|CVE-2024-10977|CVE-2024-10978|CVE-2024-53051' /tmp/results.txt; then
+    echo "ok"
+else
+    echo fail
+    exit 1
 fi
